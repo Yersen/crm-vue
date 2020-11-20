a4914ba5e7641238c9864752cbfe58160b6b6e7d
Yersen <44286140+Yersen@users.noreply.github.com>
2020-11-20 20:59:22 +0600

    Выполнены категории расходов/доходов,добавление новых записей

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './auth'
 import info from './info'
+import category from './category'
+import record from './record'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -28,6 +31,8 @@ export default new Vuex.Store({
   },
   modules: {
     auth,
-    info
+    info,
+    category,
+    record
   }
 })
