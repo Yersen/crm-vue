2a18d050fa0ba04c3b49f189b27e14aa3bce8f58
Yersen <44286140+Yersen@users.noreply.github.com>
2020-11-22 18:44:48 +0600

    Добавлена частичная локализация,пагинация на историю растра и график растрат

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,45 +5,96 @@
         </div>
 
         <div class="history-chart">
-            <canvas></canvas>
+            <canvas ref="canvas"></canvas>
         </div>
         <Loader v-if="loading" />
 
         <p class="center" v-else-if="!records.length">Записи не добавлены =><router-link to="/record">Добавьте первую запись</router-link></p>
 
         <section v-else>
-            <HistoryTable :records="records"/>
+            <HistoryTable :records="items"/>
+
+            <Paginate 
+                v-model="page"
+                :page-count="pageCount"
+                :click-handler="pageChangeHandler"
+                :prev-text="'Назад'"
+                :next-text="'Вперед'"
+                :container-class="'pagination'"
+                :page-class="'waves-effect'"
+            />
+            
         </section>
         </div>
 </template>
 
 <script>
 import HistoryTable from '@/components/app/HistoryTable'
-
+import paginationMixin from '@/mixins/pagination.mixin'
+import {Pie} from 'vue-chartjs'
 export default {
     name:'history',
+    extends: Pie,
+    mixins: [paginationMixin],
     data: () => ({
         loading:true,
         records: [],
-        categories: [],
     }),
     async mounted() {
-        //this.records = await this.$store.dispatch('fetchRecords')
-        const records = await this.$store.dispatch('fetchRecords')
-        this.categories = await this.$store.dispatch('fetchCategories')
-        this.records = records.map(record => {
-            return {
-                ...record,
-                categoryName: this.categories.find(c => c.id === record.categoryID).title,
-                typeClass: record.type === 'income' ? 'green' : 'red',
-                typeText: record.type === 'income' ? 'Доход' : 'Расход',
-            }
-        })
+        this.records = await this.$store.dispatch('fetchRecords')
+       
+        const categories = await this.$store.dispatch('fetchCategories')
+        
+        this.setup(categories)
 
         this.loading = false
     },
+    methods: {
+        setup(categories) {
+            this.setupPagination(this.records.map(record => {
+                return {
+                    ...record,
+                    categoryName: categories.find(c => c.id === record.categoryID).title,
+                    typeClass: record.type === 'income' ? 'green' : 'red',
+                    typeText: record.type === 'income' ? 'Доход' : 'Расход',
+                }
+            }))       
+
+            this.renderChart({
+                labels: categories.map(c => c.title),
+                datasets: [{
+                label: 'Расходы по категориям',
+                data: categories.map(c => {
+                    return this.records.reduce((total, r) => {
+                        if(r.categoryID === c.id && r.type === 'outcome'){
+                            total += r.amount
+                        }
+                        return total
+                    },0)
+                }),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+                }]
+            })    
+        }
+    },
     components: {
-        HistoryTable
+        HistoryTable,
     },
 }
 </script>
